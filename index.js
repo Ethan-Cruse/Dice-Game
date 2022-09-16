233f2d5223060b5482b6a25ee7f79aff1bbf2cf6
Ethan-Cruse <ethanhcruse@gmail.com>
2022-09-16 23:27:00 +1000

    DiceGame
    Have updated JS, made random number gen, just have to link statements.

---
+++ index.js
@@ -0,0 +1,22 @@
+
+var imgFirst = document.querySelector(".img1");
+var imgSecond = document.querySelector(".img2");
+
+var randomNumber1 = (Math.ceil(Math.random()*6));
+console.log (randomNumber1);
+
+if (randomNumber1 === 1) {
+    element.setAttribute("imgFirst", src="images/dice1.png");
+} else if (randomNumber1 === 2) {
+    element.setAttribute("imgFirst", src="images/dice1.png");
+} else if (randomNumber1 === 3) {
+    element.setAttribute("imgFirst", src="images/dice1.png");
+} else if (randomNumber1 === 4) {
+    element.setAttribute("imgFirst", src="images/dice1.png");
+} else if (randomNumber1 === 5) {
+    element.setAttribute("imgFirst", src="images/dice1.png");
+} else if (randomNumber1 === 6) {
+    element.setAttribute("imgFirst", src="images/dice1.png");
+}
+
+document.querySelector(".img1").setAttribute("src","https://longreadsblog.files.wordpress.com/2018/01/gettyimages-89186539.jpg");
